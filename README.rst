53aec5279996c7c772fdd43019c24e7c9c749403
Julien Hautefeuille <julien@hautefeuille.eu>
2013-06-26 17:15:14 +0200

    Write doc, cleaner code, better example

--- README.rst
+++ README.rst
@@ -1,5 +1,18 @@
-Gauge
-=====
+    Segment class
+    #############
 
-The class `Gauge` widget is a widget for displaying gauge. 
+    The class`Segment` widget is a widget for displaying segment in Kivy.
 
+    The value property of segment must be a string.
+    The scale property of segment must be a float.
+    The color property of segment must be a string.
+
+    Ex::
+
+    seg = Segment(scale=0.3, value="A.")
+
+    Are permitted : 0 1 2 3 4 5 6 7 8 9 and 0. 1. 2. 3. 4. 5. 6. 7. 8. 9.
+
+    and
+
+    A b C d E F and A. b. C. d. E. F.
--- __init__.py
+++ __init__.py
@@ -6,58 +6,76 @@
 Segment
 =======
 
-The :class:`Segment` widget is a widget for displaying segment. 
+The :class:`Segment` widget is a widget for displaying segment.
+
+The value property of segment must be a string.
+The scale property of segment must be a float.
+The color property of segment must be a string.
+
+Ex::
+
+seg = Segment(scale=0.3, value="A.")
+
+Are permitted : 0 1 2 3 4 5 6 7 8 9 and 0. 1. 2. 3. 4. 5. 6. 7. 8. 9.
+
+and
+
+A b C d E F and A. b. C. d. E. F.
 
 '''
 
-__all__ = ('Segment')
+__all__ = ('Segment',)
 
 __title__ = 'garden.segment'
 __version__ = '0.2'
 __author__ = 'julien@hautefeuille.eu'
 
-import random
 import kivy
 kivy.require('1.7.1')
 from kivy.config import Config
 from kivy.app import App
-from kivy.clock import Clock
-from kivy.properties import NumericProperty
 from kivy.properties import StringProperty
 from kivy.properties import BoundedNumericProperty
-from kivy.properties import ListProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.relativelayout import RelativeLayout
-from kivy.uix.widget import Widget
-from kivy.uix.scatter import Scatter
-from kivy.uix.image import Image
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.progressbar import ProgressBar
-from kivy.graphics import (Color, Ellipse, Line, Rectangle, Mesh, Scale, 
-Translate, PopMatrix, PushMatrix)
+from kivy.graphics import Color, Ellipse, Mesh, Scale
 from kivy.utils import get_color_from_hex
-from kivy.graphics.transformation import Matrix
-from kivy.uix.stencilview import StencilView
+
 
 class Segment(RelativeLayout):
     '''
     Segment class
 
+    The class`Segment` widget is a widget for displaying segment.
+
+    The value property of segment must be a string.
+    The scale property of segment must be a float.
+    The color property of segment must be a string.
+
+    Ex::
+
+    seg = Segment(scale=0.3, value="A.")
+
+    Are permitted : 0 1 2 3 4 5 6 7 8 9 and 0. 1. 2. 3. 4. 5. 6. 7. 8. 9.
+
+    and
+
+    A b C d E F and A. b. C. d. E. F.
+
     '''
 
-    scale = NumericProperty(0.1, min=0.1, max=1, errorvalue=0.2)
+    # Object properties configuration
+    scale = BoundedNumericProperty(0.1, min=0.1, max=1, errorvalue=0.2)
     color = StringProperty('2fc827')
-    value = StringProperty("0")
+    value = StringProperty('A.')
 
     def __init__(self, **kwargs):     
         super(Segment, self).__init__(**kwargs)
 
-        self.indice = xrange(0,6)
+        # Drawing meshes configuration, indices range meshes and mode
+        self.indice = xrange(0, 6)
         self.xmode = 'triangle_fan'
         
+        # Segment matrix configuration
         seg_1 = [
                 20, 215, 0, 0,
                 35, 230, 0, 0,
@@ -115,6 +133,7 @@ class Segment(RelativeLayout):
                 35, 0, 0, 0,
                 ]
 
+        # Drawing association
         type_0 = [seg_1, seg_2, seg_3, seg_5, seg_6, seg_7]
         type_1 = [seg_3, seg_6]
         type_2 = [seg_1, seg_3, seg_4, seg_5, seg_7]
@@ -132,6 +151,7 @@ class Segment(RelativeLayout):
         type_E = [seg_1, seg_2, seg_4, seg_5, seg_7]
         type_F = [seg_1, seg_2, seg_4, seg_5]
 
+        # Routing association
         self.type_dic = {
                 "0" : type_0,
                 "0.": type_0,
@@ -161,54 +181,79 @@ class Segment(RelativeLayout):
                 "C.": type_C,
                 "d" : type_d,
                 "d.": type_d,
-                "E": type_E,
+                "E" : type_E,
                 "E.": type_E,
                 "F" : type_F,
                 "F.": type_F,
                 }
 
+        # Binding refresh drawing method
         self.bind(
             pos=self._update_canvas, 
             size=self._update_canvas,
             value=self._update_canvas
             )
-        
-        Clock.schedule_interval(self.refresh, 0.5)
 
     def _update_canvas(self, *args):
 
         with self.canvas:
+
+            # Refresh
             self.canvas.clear()
+
+            # Configure
             Color(
                 get_color_from_hex(self.color)[0], 
                 get_color_from_hex(self.color)[1], 
                 get_color_from_hex(self.color)[2], 100)
+
+            # Scale
             Scale(self.scale)
 
+            # Draw meshes
             def make_mesh(self, ttype, *args):
+                ''' Drawing meshes
+                '''
                 for segment in ttype:
-                    Mesh(vertices=segment, indices=self.indice, mode=self.xmode)
+                    Mesh(
+                        vertices=segment, 
+                        indices=self.indice, 
+                        mode=self.xmode
+                        )
                 if len(self.value) > 1:
-                    thept = Ellipse(pos=(135, 0), size=(25,25), segments=360)
+                    Ellipse(
+                        pos=(135, 0), 
+                        size=(25,25), 
+                        segments=360
+                        )
 
+            # Avoid if session
             for key, val in self.type_dic.items():
                 if self.value == key:
                     make_mesh(self, ttype=val)
-   
-    def refresh(self, *args):
-            self.value = str(random.choice('123456789AbCdEF'))
+      
 
-        
 class SegmentTestApp(App):
 
     def build(self):
+        from kivy.clock import Clock
+        from kivy.uix.gridlayout import GridLayout
+        import random
+
+        def refresh_task(self, *args):
+            seg.value = random.choice('123AbCdEF')
+            seg1.value = random.choice('123')
 
         box = GridLayout(cols=8, padding=20)
-        self.seg = Segment(scale=0.3, value="9")
-        self.seg1 = Segment(scale=0.3, value="9")
-        box.add_widget(self.seg)
-        box.add_widget(self.seg1)
+        seg = Segment(scale=0.3, value="9.")
+        seg1 = Segment(scale=0.8, value="A.")
+        
+        box.add_widget(seg)
+        box.add_widget(seg1)
+
+        Clock.schedule_interval(refresh_task, 1)
+
         return box
             
-if __name__ in ('__main__'):
+if __name__ == '__main__':
     SegmentTestApp().run()
